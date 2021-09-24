0c0868032d85678ba6484dc7fb18611e58ebe271
Kloak Information Technologies Inc <info@kloak.app>
2021-09-23 17:19:07 -0700

    first

--- dist/GateWay/log.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.logger = void 0;
-const logger = (...argv) => {
-    const date = new Date();
-    let dateStrang = `[Gateway ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}:${date.getMilliseconds()}] `;
-    return console.log(dateStrang, ...argv);
-};
-exports.logger = logger;
--- src/GateWay/log.ts
+++
@@ -1,5 +0,0 @@
-export const logger = (...argv: any) => {
-    const date = new Date ()
-    let dateStrang = `[Gateway ${ date.getHours() }:${ date.getMinutes() }:${ date.getSeconds() }:${ date.getMilliseconds ()}] `
-    return console.log ( dateStrang, ...argv )
-}
